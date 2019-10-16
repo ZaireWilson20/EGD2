e4a7d95d540f2e08bb504db2ab6271f4221a87f0
Zaire <Zaire.R.Wilson@gmail.com>
2019-10-15 21:17:30 -0400

    auto move

--- EGD Project 2/Assets/Scripts/Player/CharacterController2D.cs
+++ EGD Project 2/Assets/Scripts/Player/CharacterController2D.cs
@@ -16,8 +16,16 @@ public class CharacterController : MonoBehaviour
 
     }
 
-    public static void MoveCharacter(Rigidbody2D obj, Vector3 origin, Vector3 movementVect){
+    public static void MoveCharacter(Rigidbody2D obj, Vector3 origin, Vector3 movementVect)
+    {
         obj.MovePosition(origin + movementVect);
 
+    }
+
+
+    public static Vector3 MoveCharacterTowards(Vector3 origin, Vector3 target, float speed)
+    {
+        //public static void MoveCharacter(Rigidbody2D obj, Vector3 origin, )
+        return Vector3.MoveTowards(origin, target, speed*Time.deltaTime);
     }
 }
--- EGD Project 2/Assets/Scripts/Player/NPC.cs
+++ EGD Project 2/Assets/Scripts/Player/NPC.cs
@@ -5,14 +5,68 @@ using UnityEngine;
 public class NPC : MonoBehaviour
 {
     // Start is called before the first frame update
+    [SerializeField]
+    float movementSpeed;
+
+    [SerializeField]
+    Transform[] movementPoints;
+
+    [SerializeField]
+    private bool cont = true;
+
+    [SerializeField]
+    float secondsTilNextMove = 2f; 
+    Vector3 currentPosition;
+    bool colliding = false; 
+
+
+    Rigidbody2D npcRb; 
+
+
     void Start()
     {
-        
+        npcRb = this.gameObject.GetComponent<Rigidbody2D>(); 
+        int ran = Random.Range(0, movementPoints.Length);
+        transform.position = movementPoints[ran].position;
+        currentPosition = transform.position;
+        StartCoroutine(randomMove());
+
     }
 
+
+    public IEnumerator randomMove()
+    {
+        while (cont)
+        {
+            int newPos = Random.Range(0, movementPoints.Length);
+
+            currentPosition = movementPoints[newPos].position;
+            yield return new WaitForSeconds(secondsTilNextMove);
+        }
+    }
     // Update is called once per frame
     void Update()
     {
-        
+        if (transform.position != currentPosition && !colliding)
+        {
+            transform.position = CharacterController.MoveCharacterTowards(transform.position, currentPosition, movementSpeed);
+        }
+
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.transform.tag == "Player")
+        {
+            colliding = true;
+        }
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.transform.tag == "Player")
+        {
+            colliding = false;
+        }
     }
 }
