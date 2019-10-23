cb5251527d201471e2b15424a9bf9cf7d3b6c551
Kyler Kubo <kubok@rpi.edu>
2019-10-22 21:11:11 -0400

    This is the real version

--- EGD Project 2/Assets/ReadyToQuit.cs
+++ EGD Project 2/Assets/ReadyToQuit.cs
@@ -1,13 +1,21 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class ReadyToQuit : MonoBehaviour
 {
+
+    public GameObject yesButton;
+    public GameObject  noButton;
+    public GameObject leaveText;
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        yesButton.SetActive(false);
+        noButton.SetActive(false);
     }
 
     // Update is called once per frame
@@ -16,8 +24,24 @@ public class ReadyToQuit : MonoBehaviour
         
     }
 
-    void OnCollisionEnter2d()
+    void OnCollisionEnter2D(Collision2D collision)
     {
+        Time.timeScale = 0;
+        leaveText.SetActive(true);
+        yesButton.SetActive(true);
+        noButton.SetActive(true);
+    }
 
+    public void GoMainMenu()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    public void NoButton()
+    {
+        Time.timeScale = 1;
+        yesButton.SetActive(false);
+        noButton.SetActive(false);
+        leaveText.SetActive(false);
     }
 }
