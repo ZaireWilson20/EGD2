73087e81273a4a8711c49bf5532b920c65a544b5
Kyler Kubo <kubok@rpi.edu>
2019-10-22 20:47:13 -0400

    Intro/MainMenu functionality

---
+++ EGD Project 2/Assets/Fade.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fade : MonoBehaviour
+{
+    
+    private MainMenu menu; 
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        menu = GameObject.FindObjectOfType<MainMenu>(); 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void FadeDone()
+    {
+        menu.doneFading = true; 
+    }
+}
--- EGD Project 2/Assets/Scripts/MainMenu.cs
+++ EGD Project 2/Assets/Scripts/MainMenu.cs
@@ -6,6 +6,15 @@ using UnityEngine.UI;
 
 public class MainMenu : MonoBehaviour
 {
+
+    public Animator fade;
+    public GameObject intro;
+    public GameObject StartBut;
+    public GameObject QuitBut; 
+
+    public bool doneFading = false;
+
+    public bool dontDoThis = false;
     // Start is called before the first frame update
     void Start()
     {
@@ -15,17 +24,33 @@ public class MainMenu : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (doneFading && !dontDoThis)
+        {
+            intro.SetActive(true);
+            fade.SetBool("Done", true);
+            Invoke("ChangeScenes", 5f);
+            dontDoThis = true; 
+
+        }
     }
 
     public void QuitButton()
     {
+
         Application.Quit();
     }
 
     public void StartButton()
     {
-        SceneManager.LoadScene(1);
+        fade.SetBool("FadeIn", true);
+        StartBut.SetActive(false);
+        QuitBut.SetActive(false);
+        //SceneManager.LoadScene(4);
+    }
+
+    private void ChangeScenes()
+    {
+        SceneManager.LoadScene(4);
     }
 
 }
